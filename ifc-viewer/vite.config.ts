c58c79b5b37f0e88911656db743d78803d118863
Jan Brouwer <jan@brewsky.nl>
2026-03-22 16:11:49 +0000

    set dynamic base path

--- ifc-viewer/vite.config.ts
+++ ifc-viewer/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/IfcMaterialEnrichment/',
+  base: process.env.BASE_PATH ?? '/',
   plugins: [react()],
   optimizeDeps: {
     entries: ['src/**/*.{ts,tsx}', 'index.html'],
